1320a9efc968f4b7d5a69867b0227602d5b523a3
claystation0 <originallycopied7@gmail.com>
2025-09-07 23:39:33 -0400

    Update Overview_|_Persuasion_Survey.py

--- Overview_|_Persuasion_Survey.py
+++ Overview_|_Persuasion_Survey.py
@@ -1,26 +1,31 @@
 import streamlit as st
+from streamlit_sortables import sort_items
+from pages.tools.utils import custom_style
 
-st.title("Persuasion Survey: Overview")
+st.title("The Art of Persuasion: Evaluating the Impact of Ethos, Pathos, and Logos on Argument Effectiveness")
 
 st.markdown("""
-- Student Researcher: Sabrina Mazumder
-- Title of Project: The Art of Persuasion: Evaluating theImpact of Ethos, Pathos, and Logos on Argument Effectiveness
-- Purpose of Project: The goal of the study is to determine which types of arguments are most convincing.
-- If You Participate, You Will Be Asked to: You will be asked to share personal data including gender, age, religion, poltical beliefs, openness,
+- **Student Researcher**: Sabrina Mazumder. For questions or concerns, please contact me at [dylansabrina@gmail.com](mailto:dylansabrina@gmail.com).
+- **Purpose of Project**: The goal of the study is to answer these two questions:
+  - Between logos, ethos, and pathos, which rhetorical appeal is most effective at changing people’s minds when used by generative artificial intelligence? 
+  - Does the persuasive impact of these appeals vary when the arguments built from them are personalized?
+- **If You Participate, You Will Be Asked to**: You will be asked to share personal data including gender, age, religion, poltical beliefs, openness,
             personal values, and opinions.
-- Time Required for Participation: ~25 minutes
-- Risks: This survey includes exposure to arguments created by generative artificial intelligence that may contain
-            bias, emotionally persuasive language, and factual inaccuracies. I will collect limited personal data to
-            contextualize responses. These responses will be stored securely, but as with any online activity, there is the
-            risk of a data breach. Some participants may experience discomfort or psychological stress when engaging
-            with certain arguments.
-- Benefits: There are no direct benefits to you.
-- How Confidentiality Will Be Maintained: The survey is anonymized and asks for no personally identifiable information.
-            Only the primary researcher will have access to the raw dataset. Results will only be reported in aggregate form,
-            ensuring no individual participant will be identified.
-By checking the box below, you confirm that you understand the information provided and agree to participate in the survey.
+- **Time Required for Participation**: ~15 minutes
+- **Risks**: This survey includes exposure to arguments created by generative artificial intelligence that may contain
+            bias, emotionally persuasive language, and factual inaccuracies. Some participants may experience discomfort or psychological stress when engaging
+            with certain arguments. I will collect limited personal data to contextualize responses. 
+            These responses will be stored securely, but as with any online activity, there is the
+            risk of a data breach.
+- **Benefits**: There are no direct benefits to you. However, you may find the survey questions interesting, 
+            and your responses contribute to the growing body of research pertaining to generative artificial intelligence's persuasive capabilities.
+- **How Confidentiality Will Be Maintained**: The survey data is anonymized and solely for research purposes. 
+            Results will only be reported in aggregate form, ensuring no individual participant will be identified.
+
+By checking the box below, you confirm that you understand the above information and agree to participate in the survey.
 """)
+
 consent = st.checkbox("I consent")
 if consent:
     if st.button("Next", type="primary"):
-        st.switch_page("pages/Contextual_Data_|_Persuasion_Survey.py")
+        st.switch_page("pages/Instructions.py")

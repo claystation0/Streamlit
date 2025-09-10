d3ac107ed3b9edf4263b52683fc1960d9cfdd0fb
claystation0 <originallycopied7@gmail.com>
2025-09-09 22:14:00 -0400

    Update Instructions.py

--- pages/Instructions.py
+++ pages/Instructions.py
@@ -10,7 +10,7 @@ st.markdown("""
 - For each question:
   1. Please indicate whether you agree with, disagree with, or feel neutral about the given statement.
 
-  2. Then, rank the four arguments from most to least convincing.
+  2. Rank the four arguments from most to least convincing.
                 
   3. Given your initial stance, please indicate on the scale the degree to which the arguments have changed your position, if at all.
             1 means that your stance has not changed at all as a result of the arguments, 5 means your stance has definitely changed as
@@ -18,4 +18,4 @@ st.markdown("""
 """)
 
 if st.button("Next", type="primary"):
-    st.switch_page("pages/Contextual_Data.py")
+  st.switch_page("pages/Contextual_Data.py")
